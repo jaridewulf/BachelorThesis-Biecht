94e51313eb49e2c871b97e801b7894a05c7c3c3a
Jari De Wulf <jari.de.wulf@student.howest.be>
2023-12-02 19:25:29 +0100

    add template

---
+++ phone/phoneHandler.py
@@ -0,0 +1,43 @@
+def greetUser():
+    # Play greeting audio
+    print('greet user')
+
+def getDepartment():
+    # Prompt user to select department
+    print('get department')
+
+def getUserFeedback():
+    # Ask user about feedback about past event
+    print('give feedback')
+
+def getFeedbackStatus():
+    # Ask if the feedback positive or negative
+    print('get status')
+
+def getFeedbackSeverity():
+    # Ask if the feedback positive or negative
+    print('get severity')    
+
+def playAudioBasedOnFeedback():
+    # Play audio based on feedback
+    print('playing audio')
+
+def saveData():
+    # Data sent to backend
+    print('saving data')
+
+def resetProgram():
+    # Implement code to reset the program state
+    print('reseting program')
+
+greetUser()
+resetProgram()
+# Set department
+department = getDepartment()
+getUserFeedback()
+# Get feedbackStatus
+feedbackStatus = getFeedbackStatus()
+getFeedbackSeverity()
+playAudioBasedOnFeedback()
+saveData()
+resetProgram()
