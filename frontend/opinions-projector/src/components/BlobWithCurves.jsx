83b29482c6d508983d80831c2fa9ead714d819ad
Jari <jari.de.wulf1999@gmail.com>
2023-12-07 21:32:34 +0100

    Add data  API & auto tension(pos||neg) #71 #72 #73

--- frontend/opinions-projector/src/components/BlobWithCurves.jsx
+++ frontend/opinions-projector/src/components/BlobWithCurves.jsx
@@ -1,31 +1,62 @@
 import { CardinalClosedCurve } from "react-svg-curve";
+import { useEffect, useState } from "react";
 
-const BlobWithCurves = () => {
-  const data = [3, 3, 7, 3, 5, 6, 6, 7];
-
+const BlobWithCurves = ({ feedbackData }) => {
   // Variables
   const width = 400;
   const height = 400;
   const center = { x: width / 2, y: height / 2 };
   const maxRadius = Math.max(width, height) / 2 - 10;
-  // Will show severity
-  const tension = 0.2;
   const fill = "rgba(0, 120, 255, 1)";
+
+  // Set Data
+  const [intensity, setIntensity] = useState([]);
+  const [tensionValue, setTensionValue] = useState(0);
+
+  // Calculate tension
+  const calculateTension = (sentiment) => {
+    if (sentiment === "NEGATIVE") {
+      setTensionValue((prevTensionValue) => {
+        const newTensionValue = prevTensionValue + 1;
+        return newTensionValue;
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (feedbackData.length >= 3) {
+      setIntensity([]);
+      setTensionValue(0);
+      // Use map to create an array of intensities
+      const intensitiesArray = feedbackData.map(feedback => {
+        feedback.intensity;
+        calculateTension(feedback.sentiment);
+        return feedback.intensity;
+      });
+
+      // Use spread operator to concatenate the existing intensity state with the new values
+      setIntensity(prevIntensity => [...prevIntensity, ...intensitiesArray]);
+      setTensionValue((prevTensionValue) => {
+        const newTensionValue = prevTensionValue / feedbackData.length;
+        return newTensionValue;
+      });
+    }
+  }, [feedbackData]);
 
   // Function to scale data values to a radius range
   const scaleValueToRadius = (value, max) => Math.round((value / 9) * max);
 
   // Convert data values to scaled coordinates
-  const coordinates = data.map((value, index) => [
+  const coordinates = intensity.map((value, index) => [
     Math.round(
       center.x +
-        scaleValueToRadius(value, maxRadius) *
-          Math.cos(index * ((2 * Math.PI) / data.length))
+      scaleValueToRadius(value, maxRadius) *
+      Math.cos(index * ((2 * Math.PI) / intensity.length))
     ),
     Math.round(
       center.y +
-        scaleValueToRadius(value, maxRadius) *
-          Math.sin(index * ((2 * Math.PI) / data.length))
+      scaleValueToRadius(value, maxRadius) *
+      Math.sin(index * ((2 * Math.PI) / intensity.length))
     ),
   ]);
 
@@ -33,7 +64,7 @@ const BlobWithCurves = () => {
     <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
       <CardinalClosedCurve
         data={coordinates}
-        tension={tension}
+        tension={tensionValue}
         fill={fill}
         showPoints={false}
         strokeWidth={0}
