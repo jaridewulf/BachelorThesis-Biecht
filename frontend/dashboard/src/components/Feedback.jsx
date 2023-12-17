32bf3176d0672b7c7fc4f64ebb4d78ac457ce221
Jari <jari.de.wulf1999@gmail.com>
2023-12-17 02:04:10 +0100

    Clean up code

--- frontend/dashboard/src/components/Feedback.jsx
+++ frontend/dashboard/src/components/Feedback.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString();
+};
+
 
 const Feedback = ({ feedback }) => {
     return (
         <div>
-            <p>ID: {feedback.id}</p>
-            <p>Created At: {feedback.createdAt}</p>
-            <p>Intensity: {feedback.intensity}</p>
-            <p>Sentiment: {feedback.sentiment}</p>
-            <p>Department ID: {feedback.departmentId}</p>
-            <p>Location ID: {feedback.locationId}</p>
+            <h3>Open question</h3>
             <audio controls>
                 <source src={feedback.audioUrls.open} type="audio/mp3" />
             </audio>
+            <h3>Closed question</h3>
             <audio controls>
                 <source src={feedback.audioUrls.closed} type="audio/mp3" />
             </audio>
+            <p><b>ID</b> {feedback.id}</p>
+            <p><b>Created At</b> {formatDate(feedback.createdAt)}</p>
+            <p><b>Intensity</b> {feedback.intensity}</p>
+            <p><b>Sentiment</b> {feedback.sentiment}</p>
+            <p><b>Department ID</b> {feedback.departmentId}</p>
+            <p><b>Location ID</b> {feedback.locationId}</p>
+            <p>______________________________________________________________</p>
         </div>
     );
 };
