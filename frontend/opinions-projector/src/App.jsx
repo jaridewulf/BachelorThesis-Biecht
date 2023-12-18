cb3e6d5eb2af1850123c1f38e8caa8643cc53cf5
Jari <jari.de.wulf1999@gmail.com>
2023-12-18 21:30:23 +0100

    Create different blobs with departmentId

--- frontend/opinions-projector/src/App.jsx
+++ frontend/opinions-projector/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import BlobWithCurves from "./components/BlobWithCurves";
 
+import { useRef } from "react";
+
 function App() {
   const [feedbackData, setFeedbackData] = useState([]);
 
@@ -19,10 +21,38 @@ function App() {
     fetchData();
   }, []);
 
+  const groupedFeedbackData = feedbackData.reduce((acc, item) => {
+    // Check if there is an existing group with the same departmentId
+    const existingItem = acc.find((group) => group.departmentId === item.departmentId);
+
+    if (existingItem) {
+      // If an existing group is found, push the item to its items array
+      existingItem.items.push(item);
+    } else {
+      // If no existing group is found, create a new group with the departmentId and items array
+      acc.push({ departmentId: item.departmentId, items: [item] });
+    }
+
+    return acc;
+  }, [feedbackData]);
+
+  console.log(groupedFeedbackData);
+
   return (
     <>
-      <div>
+      <div style={{ position: "relative" }}>
         <BlobWithCurves feedbackData={feedbackData} />
+        {groupedFeedbackData.slice(1).map((group, index) => (
+          <BlobWithCurves
+            key={group.departmentId}
+            feedbackData={group.items}
+            style={{
+              position: "absolute",
+              top: `${50 + 100 * Math.sin((2 * Math.PI * index) / groupedFeedbackData.length)}%`,
+              left: `${50 + 100 * Math.cos((2 * Math.PI * index) / groupedFeedbackData.length)}%`,
+            }}
+          />
+        ))}
       </div>
     </>
   );
